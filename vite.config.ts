c1cd0e000c516dd6c945e61971b759e4a6cbad61
BadCoder <badcoder.of@gmail.com>
2026-06-07 21:15:00 +0200

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	base: '/prism-3d-prod/',
+  plugins: [react()],
+  base: './'
 })
