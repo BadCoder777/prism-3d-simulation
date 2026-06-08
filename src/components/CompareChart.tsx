b2fbaaafc21befd04b62532f0ace90b9da26253e
BadCoder <badcoder.of@gmail.com>
2026-06-08 20:27:03 +0200

    fix bottom-padding-2

--- src/components/CompareChart.tsx
+++ src/components/CompareChart.tsx
@@ -7,93 +7,93 @@ import { calcIMoments } from '../utils/calcIMoments'
 import { eulerSolver } from '../utils/eulerSolver'
 
 export const CompareChart = () => {
-	const [currentFile] = useAtom(currentPlayingFile)
+  const [currentFile] = useAtom(currentPlayingFile)
 
-	const [args] = useAtom(chartArguments)
-	const myDivRef = useRef<HTMLDivElement>(null)
-	const [I_x, I_y, I_z] = useMemo(() => {
-		// @ts-ignore
-		return calcIMoments([args.a, args.b, args.c, args.m])
-	}, [args])
+  const [args] = useAtom(chartArguments)
+  const myDivRef = useRef<HTMLDivElement>(null)
+  const [I_x, I_y, I_z] = useMemo(() => {
+    // @ts-ignore
+    return calcIMoments([args.a, args.b, args.c, args.m])
+  }, [args])
 
-	const compareUPlotInst = useRef<uPlot | null>(null)
+  const compareUPlotInst = useRef<uPlot | null>(null)
 
-	const compareChartRef = useRef<HTMLDivElement>(null)
+  const compareChartRef = useRef<HTMLDivElement>(null)
 
-	useEffect(() => {
-		if (!compareChartRef.current) return
+  useEffect(() => {
+    if (!compareChartRef.current) return
 
-		if (compareUPlotInst.current) compareUPlotInst.current.destroy()
+    if (compareUPlotInst.current) compareUPlotInst.current.destroy()
 
-		const opts: uPlot.Options = {
-			width: compareChartRef.current.clientWidth,
-			height: myDivRef.current.clientHeight,
-			cursor: {
-				drag: { x: true, y: false },
-			},
-			scales: {
-				x: { time: false },
-				y: { range: [-100, 100] },
-			},
-			series: [
-				{},
-				{
-					label: 'wx',
-					stroke: '#ffe924f0',
-					width: 2,
-				},
-				{
-					label: 'wy',
-					stroke: '#2453ffe8',
-					width: 2,
-				},
-				{
-					label: 'wz',
-					stroke: '#48ff24e8',
-					width: 2,
-				},
-			],
-			axes: [
-				{
-					stroke: '#000000',
-					grid: { stroke: '#aba9a9' },
-				},
-				{
-					stroke: '#000000',
-					grid: { stroke: '#b3b1b1' },
-				},
-			],
-		}
-		compareUPlotInst.current = new uPlot(
-			opts,
-			// @ts-ignore
-			eulerSolver(
-				I_x,
-				I_y,
-				I_z,
-				// @ts-ignore
-				args.wx,
-				// @ts-ignore
-				args.wy,
-				// @ts-ignore
-				args.wz,
-				// @ts-ignore
-				args.dt,
-				// @ts-ignore
-				args.time
-			),
-			compareChartRef.current
-		)
-	}, [args, I_x, I_y, I_z])
+    const opts: uPlot.Options = {
+      width: compareChartRef.current.clientWidth,
+      height: myDivRef.current.clientHeight,
+      cursor: {
+        drag: { x: true, y: false }
+      },
+      scales: {
+        x: { time: false },
+        y: { range: [-100, 100] }
+      },
+      series: [
+        {},
+        {
+          label: 'wx',
+          stroke: '#ffe924f0',
+          width: 2
+        },
+        {
+          label: 'wy',
+          stroke: '#2453ffe8',
+          width: 2
+        },
+        {
+          label: 'wz',
+          stroke: '#48ff24e8',
+          width: 2
+        }
+      ],
+      axes: [
+        {
+          stroke: '#000000',
+          grid: { stroke: '#aba9a9' }
+        },
+        {
+          stroke: '#000000',
+          grid: { stroke: '#b3b1b1' }
+        }
+      ]
+    }
+    compareUPlotInst.current = new uPlot(
+      opts,
+      // @ts-ignore
+      eulerSolver(
+        I_x,
+        I_y,
+        I_z,
+        // @ts-ignore
+        args.wx,
+        // @ts-ignore
+        args.wy,
+        // @ts-ignore
+        args.wz,
+        // @ts-ignore
+        args.dt,
+        // @ts-ignore
+        args.time
+      ),
+      compareChartRef.current
+    )
+  }, [args, I_x, I_y, I_z])
 
-	return (
-		<div
-			ref={myDivRef}
-			className={`${
-				currentFile != '' ? 'w-[47.5%]' : 'w-[96%] left-6'
-			} bg-[#E9E8EF] h-[37%] rounded-3xl absolute left-6 pb-5`}
-		>
-			<div ref={compareChartRef}></div>
-		</div>
-	)
+  return (
+    <div
+      ref={myDivRef}
+      className={`${
+        currentFile != '' ? 'w-[47.5%]' : 'w-[96%] left-6'
+      } bg-[#E9E8EF] h-[36%] rounded-3xl absolute left-6 pb-5`}
+    >
+      <div ref={compareChartRef}></div>
+    </div>
+  )
 }
